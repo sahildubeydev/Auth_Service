2668ac17676b289cb55cc5e1c5109aba0839a5e9
Sahil Dubey <dsahil.contact@gmail.com>
2023-03-02 00:43:05 +0530

    Added get user by Id in repo

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -24,5 +24,17 @@ class UserRepository {
       throw error;
     }
   }
+
+  async getById(userId) {
+    try {
+      const user = await User.findByPk(userId, {
+        attributes: ["email", "id"],
+      });
+      return user;
+    } catch (error) {
+      console.log("Something went wrong inside user-repository");
+      throw error;
+    }
+  }
 }
 module.exports = UserRepository;
